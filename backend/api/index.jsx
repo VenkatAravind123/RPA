69143892ed6b62efba40f47ae89627e789fd479a
Venkat Aravind <venkataravind.123456@gmail.com>
2025-02-16 19:19:18 +0530

    Frontend Updated

--- backend/api/index.jsx
+++ backend/api/index.jsx
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-  origin: 'http://localhost:3000', // Replace with your frontend URL
+  origin: 'https://rpa-club-two.vercel.app', // Replace with your frontend URL
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 }));
